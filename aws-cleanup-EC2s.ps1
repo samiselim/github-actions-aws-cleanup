7c6dd4f2324a4a0d9b7095a37f12fae142efd3dd
sami abdelaziz selim <samiselim75@gmail.com>
2024-06-08 12:55:34 +0300

    Edit pswh command of deleteing ec2s | adding ()

--- aws-cleanup-EC2s.ps1
+++ aws-cleanup-EC2s.ps1
@@ -1,3 +1,3 @@
 #CleanUp Ec2 Instances 
-Get-EC2Instance.Instances | % { Remove-EC2Instance -InstanceId $PSItem.InstanceId -Force }
+(Get-EC2Instance).Instances | % { Remove-EC2Instance -InstanceId $PSItem.InstanceId -Force }
 
